e20e384e8bc845062442c7e4a1469d01c0b55d0d
Erick Gonzalez <erick.gonzalez@omnipresent.com>
2022-12-16 23:19:42 +0100

    turn on ssl rej unauth

--- dbconfig/index.ts
+++ dbconfig/index.ts
@@ -11,7 +11,10 @@ export const appDataSource = new DataSource({
     type: "postgres",
     host: "localhost",
     port: 5432,
-    ssl: true,
+    ssl:
+    process.env.NODE_ENV === 'production'
+      ? { rejectUnauthorized: false }
+      : false,
     url: process.env.DATABASE_URL ?? undefined,
     username: process.env.DB_USERNAME ?? undefined,
     password: process.env.DB_PASSWORD ?? undefined,
