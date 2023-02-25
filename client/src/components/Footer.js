b367e104297deb63625c44539647fb8d197f5489
Erick Gonzalez <erick.gonzalez@omnipresent.com>
2023-02-25 19:43:17 +0100

    Adds portfolio URL to footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <StyledFooter>
       <span>
-        Erick Gonzalez
+        Erick Gonzalez <span><a href="https://gonze.dev" target={"_blank"} rel="noreferrer">gonze.dev</a></span>
         <span id="github-icon">
           <a href="https://github.com/eg180" target={"_blank"} rel="noreferrer"><img src="./images/github-octocat.svg" alt="Erick's Github profile" /></a>
           <a href="https://linkedin.com/in/mrerickgonzalez" target={"_blank"} rel="noreferrer"><img src="./images/linkedin-icon.svg" alt="Erick's Linkedin profile" /></a>
